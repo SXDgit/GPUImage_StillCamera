3f11e74be8595f714c70de93cbb59bc65511bfa1
sangxiedong <Sang4590256>
2019-02-20 16:32:34 +0800

    first commit

--- GPUImage_StillCamera/ViewController.m
+++ GPUImage_StillCamera/ViewController.m
@@ -7,8 +7,13 @@
 //
 
 #import "ViewController.h"
+#import "GPUImage.h"
+#import <AssetsLibrary/AssetsLibrary.h>
 
 @interface ViewController ()
+
+@property (nonatomic, strong) GPUImageStillCamera *stillCamera;
+@property (nonatomic, strong) GPUImageSketchFilter *filter;
 
 @end
 
@@ -16,8 +21,51 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    [self configGPUImageView];
+    [self configButton];
 }
 
+- (void)configGPUImageView {
+    // 可视的预览view
+    GPUImageView *imageView = [[GPUImageView alloc]initWithFrame:self.view.frame];
+    imageView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    imageView.fillMode = kGPUImageFillModePreserveAspectRatio;
+    self.view = imageView;
+    
+    // 创建滤镜
+    self.filter = [[GPUImageSketchFilter alloc]init];
+    
+    // 创建Camera
+    self.stillCamera = [[GPUImageStillCamera alloc]initWithSessionPreset:AVCaptureSessionPreset640x480 cameraPosition:AVCaptureDevicePositionBack];
+    _stillCamera.outputImageOrientation = UIInterfaceOrientationPortrait;
+    
+    [_stillCamera addTarget:_filter];
+    [_filter addTarget:imageView];
+    [_stillCamera startCameraCapture];
+}
+
+- (void)configButton {
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+    [button setTitle:@"拍摄" forState:UIControlStateNormal];
+    [button setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
+    button.frame = CGRectMake(([UIScreen mainScreen].bounds.size.width - 100) / 2, [UIScreen mainScreen].bounds.size.height - 50, 100, 50);
+    [button setBackgroundColor:[UIColor whiteColor]];
+    [button addTarget:self action:@selector(buttonAction) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:button];
+}
+
+- (void)buttonAction {
+    [_stillCamera capturePhotoAsJPEGProcessedUpToFilter:_filter withCompletionHandler:^(NSData *processedJPEG, NSError *error) {
+        ALAssetsLibrary *library = [[ALAssetsLibrary alloc]init];
+        [library writeImageDataToSavedPhotosAlbum:processedJPEG metadata:self.stillCamera.currentCaptureMetadata completionBlock:^(NSURL *assetURL, NSError *error) {
+            if (error) {
+                NSLog(@"ERROR: failed");
+            }else {
+                NSLog(@"SUCCESS: saved");
+            }
+        }];
+    }];
+}
 
 @end
