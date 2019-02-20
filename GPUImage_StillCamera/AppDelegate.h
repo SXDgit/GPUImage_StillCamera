098f9a34f79eb70a8fb31eddfbf54be87133716b
sangxiedong <Sang4590256>
2019-02-20 15:24:00 +0800

    Initial Commit

---
+++ GPUImage_StillCamera/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GPUImage_StillCamera
+//
+//  Created by Sangxiedong on 2019/2/20.
+//  Copyright © 2019 ZB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GPUImage_StillCamera/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GPUImage_StillCamera
+//
+//  Created by Sangxiedong on 2019/2/20.
+//  Copyright © 2019 ZB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
